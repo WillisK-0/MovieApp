35db2e3b51abae3c907a134f68e6163cafc325df
Keith Willis <williskeith04@gmail.com>
2020-06-11 22:27:09 -0500

    initializing heroku deployment

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mustacheExpress = require("mustache-express");
 const homeRouter = require("./routes/home");
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 app.use(express.static("dummy"));
@@ -16,6 +17,6 @@ app.set("views");
 app.set("view engine", "mustache");
 app.use("/", homeRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running...");
 });
